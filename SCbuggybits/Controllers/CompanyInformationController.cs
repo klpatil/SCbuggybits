40741a9b40aadf6421078d4861d08dae8e712a60
klpatil <kpatil@horizontalintegration.com>
2018-05-11 01:03:11 +0530

    Scenario 3 - As well done!

---
+++ SCbuggybits/Controllers/CompanyInformationController.cs
@@ -0,0 +1,26 @@
+﻿using SCbuggybits.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCbuggybits.Controllers
+{
+    public class CompanyInformationController : Controller
+    {
+        // GET: CompanyInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string message)
+        {   
+            BuggyMail mail = new BuggyMail();
+            mail.SendEmail(message, "whocares-at-buggymail");
+            return View();
+        }
+    }
+}
